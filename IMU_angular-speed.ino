0d3a28c3675afe7745cf0c85669a059e3434b25c
iris-segard <75125433+iris-segard@users.noreply.github.com>
2022-05-17 17:57:24 +0200

    Add files via upload

--- IMU_angular-speed.ino
+++ IMU_angular-speed.ino
@@ -1,65 +1,39 @@
 #include <Wire.h>
 #include <MPU6050.h>
 
+
 MPU6050 mpu;
+
+double speed;
+double previous;
+double present; 
 
 void setup() 
 {
-  Serial.begin(115200);
-
-  // Initialize MPU6050
-  Serial.println("Initialize MPU6050");
-  while(!mpu.begin(MPU6050_SCALE_2000DPS, MPU6050_RANGE_2G))
-  {
-    Serial.println("Could not find a valid MPU6050 sensor, check wiring!");
-    delay(500);
-  }
-  
-  
   mpu.calibrateGyro();
   mpu.setThreshold(3);
-  checkSettings();
+  
+  speed = 0.0;
 }
 
-void checkSettings()
+int angular_speed()
 {
-  Serial.println();
-  
-  Serial.print(" * Sleep Mode:        ");
-  Serial.println(mpu.getSleepEnabled() ? "Enabled" : "Disabled");
-  
-  Serial.print(" * Clock Source:      ");
-  switch(mpu.getClockSource())
-  {
-    case MPU6050_CLOCK_KEEP_RESET:     Serial.println("Stops the clock and keeps the timing generator in reset"); break;
-    case MPU6050_CLOCK_EXTERNAL_19MHZ: Serial.println("PLL with external 19.2MHz reference"); break;
-    case MPU6050_CLOCK_EXTERNAL_32KHZ: Serial.println("PLL with external 32.768kHz reference"); break;
-    case MPU6050_CLOCK_PLL_ZGYRO:      Serial.println("PLL with Z axis gyroscope reference"); break;
-    case MPU6050_CLOCK_PLL_YGYRO:      Serial.println("PLL with Y axis gyroscope reference"); break;
-    case MPU6050_CLOCK_PLL_XGYRO:      Serial.println("PLL with X axis gyroscope reference"); break;
-    case MPU6050_CLOCK_INTERNAL_8MHZ:  Serial.println("Internal 8MHz oscillator"); break;
-  }
-  
-  Serial.print(" * Gyroscope:         ");
-  switch(mpu.getScale())
-  {
-    case MPU6050_SCALE_2000DPS:        Serial.println("2000 dps"); break;
-    case MPU6050_SCALE_1000DPS:        Serial.println("1000 dps"); break;
-    case MPU6050_SCALE_500DPS:         Serial.println("500 dps"); break;
-    case MPU6050_SCALE_250DPS:         Serial.println("250 dps"); break;
-  } 
-  
-  Serial.print(" * Gyroscope offsets: ");
-  Serial.println(mpu.getGyroOffsetZ());
-  Serial.println();
+  Vector gyro = mpu.readNormalizeGyro();
+  return gyro.ZAxis;
 }
 
-void loop()
+int linear_speed() 
 {
-  Vector normGyro = mpu.readNormalizeGyro();
+  Vector accel = mpu.readNormalizeAccel();
+  return accel.XAxis * get_time()  + speed;
+}
+
+int get_time() {
+  previous = present;
+  present = millis()
+  return (present-previous);
+}
+
+void loop() {
   
-  Serial.print(" Znorm = ");
-  Serial.println(normGyro.ZAxis);
-  
-  delay(10);
 }
